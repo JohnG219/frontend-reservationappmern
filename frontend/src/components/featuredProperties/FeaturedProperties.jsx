4c1454fa31b6989356864ffc2f8d10100ee3afe9
JohnE2199 <138347578+JohnG219@users.noreply.github.com>
2023-12-18 20:58:56 +0800

    Update FeaturedProperties.jsx

--- frontend/src/components/featuredProperties/FeaturedProperties.jsx
+++ frontend/src/components/featuredProperties/FeaturedProperties.jsx
@@ -2,7 +2,7 @@ import useFetch from "../../hooks/useFetch";
 import "./featuredProperties.css"
 
 const FeaturedProperties = () => {
-  const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
+  const { data, loading, error } = useFetch("https://backend-server-reservation.onrender.com/api/hotels?featured=true&limit=4");
   
 
   return (
