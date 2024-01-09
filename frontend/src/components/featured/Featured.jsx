cb97334ef3474753246840e75e63d0ecb11d14ff
JohnE2199 <138347578+JohnG219@users.noreply.github.com>
2024-01-10 00:17:31 +0800

    Update Featured.jsx

--- frontend/src/components/featured/Featured.jsx
+++ frontend/src/components/featured/Featured.jsx
@@ -9,7 +9,7 @@ const Featured = () => {
   return (
     <div className="featured">
       {loading ? (
-        "Loading..."
+        "Loading... please wait.. don't leave or reload the page."
       ) : (
         <>
           <div className="featuredItem">
